d4ee86e2ae31347c19099f8e7ed7d5f6ab69b2e0
YeonTaeKIM <urbanscenery@gmail.com>
2018-07-06 20:25:00 +0900

    Not finished

---
+++ src/main/java/codesquad/domain/Question.java
@@ -0,0 +1,55 @@
+package codesquad.domain;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Question {
+    private String writer;
+    private String title;
+    private String contents;
+
+    private String writeTime;
+
+
+
+    public Question(){
+
+    }
+    public Question(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getWriteTime() {
+        return writeTime;
+    }
+
+    public void setWriteTime(String writeTime) {
+        this.writeTime = writeTime;
+    }
+}
